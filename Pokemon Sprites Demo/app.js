005fe0a5862cd0078dc8f501f02c6e979921b245
Ryan Souza <ryansateles@gmail.com>
2021-11-03 19:41:29 -0300

    Adicionando todos os pokemons da 1º Geração

--- Pokemon Sprites Demo/app.js
+++ Pokemon Sprites Demo/app.js
@@ -3,7 +3,7 @@
 const container = document.querySelector('#container');
 
 let baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
-for (let i = 0; i < 10; i++) {
+for (let i = 1; i <= 151; i++) {
     const newImg = document.createElement('img');
     newImg.src = `${baseURL}${i}.png`;
     container.appendChild(newImg);

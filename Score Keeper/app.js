2da1f2c3509922054c5617bc867f417cafb119f9
Ryan Souza <ryansateles@gmail.com>
2021-11-04 23:38:17 -0300

    logica toda feita (pelo menos deveria funcionar)

--- Score Keeper/app.js
+++ Score Keeper/app.js
@@ -7,14 +7,17 @@ const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 p1Button.addEventListener('click', () => {
     if (!isGameOver) {
         p1Score += 1;
-        if (p1Score === winningScore)
+        if (p1Score === winningScore) {
             isGameOver = true;
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
+        }
         p1Display.textContent = p1Score;
     }
 })
@@ -22,8 +25,11 @@ p1Button.addEventListener('click', () => {
 p2Button.addEventListener('click', () => {
     if (!isGameOver) {
         p2Score += 1;
-        if (p2Score === winningScore)
+        if (p2Score === winningScore) {
             isGameOver = true;
+            p2Display.classList.add('winner');
+            p1Display.classList.add('loser');
+        }
         p2Display.textContent = p2Score;
     }
 })
@@ -33,7 +39,7 @@ winningScoreSelect.addEventListener('change', () => {
     reset();
 })
 
-resetButton.addEventListener('click',reset);
+resetButton.addEventListener('click', reset);
 
 function reset() {
     isGameOver = false;
@@ -41,4 +47,6 @@ function reset() {
     p2Score = 0
     p1Display.textContent = 0;
     p2Display.textContent = 0;
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner','loser');
 }
